4d5718ee3d6665257dfab6a2d4320141a0102c99
goggalore <junec177@gmail.com>
2020-07-03 15:59:27 -0700

    add comment

--- lib/prime.ex
+++ lib/prime.ex
@@ -1,6 +1,8 @@
 defmodule Prime do
   ## Wilson's theorem:
   ## any number n is a prime number <=> (n − 1)! + 1 is divisible by n
+
+  # only use this for small numbers unless you want your computer to explode
   @spec is_prime?(non_neg_integer) :: boolean
   def is_prime?(p) do
     rem(Factorial.of(p - 1) + 1, p) == 0
