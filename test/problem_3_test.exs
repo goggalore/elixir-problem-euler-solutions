e74993202ac3ae2934bcb5964a490d2b319b6f4e
goggalore <junec177@gmail.com>
2020-07-11 15:31:58 -0700

    add tests for problem 3

--- test/problem_3_test.exs
+++ test/problem_3_test.exs
@@ -1,3 +1,11 @@
 defmodule PE.PrimeFactorsTest do
-  use ExUnit.Case(), async: true
+  use ExUnit.Case, async: true
+
+  test "get the maximum prime factor of a prime, which is the prime" do
+    assert PE.Prime.Factors.max(941) == 941
+  end
+
+  test "get the maximum prime factor of 600851475143" do
+    assert PE.Prime.Factors.max(600_851_475_143) == 6857
+  end
 end
