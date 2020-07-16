95a8b0237aa1bb62b77d6a6710bb943538188ed5
goggalore <junec177@gmail.com>
2020-07-16 15:57:49 -0700

    fix tests for problem four that can potentially pass even though the code is broken

--- test/problem_4_test.exs
+++ test/problem_4_test.exs
@@ -1,19 +1,23 @@
 defmodule PE.PalindromeTest do
   use ExUnit.Case, async: true
 
-  test "checking if 9341 is a palindrome is false" do
-    assert !PE.Palindrome.is_palindrome?(9341)
+  test "checking if 4141 is a palindrome is false" do
+    assert !PE.Palindrome.is_palindrome?(4141)
   end
 
-  test "checking if 9009 is a palindrome is true" do
-    assert PE.Palindrome.is_palindrome?(9009)
+  test "checking if 900009 is a palindrome is true" do
+    assert PE.Palindrome.is_palindrome?(900_009)
   end
 
-  test "checking if 12823 is a palindrome is false" do
-    assert !PE.Palindrome.is_palindrome?(12823)
+  test "checking if 96696 is a palindrome is false" do
+    assert !PE.Palindrome.is_palindrome?(96696)
   end
 
   test "checking if 12821 is a palindrome is true" do
     assert PE.Palindrome.is_palindrome?(12821)
+  end
+
+  test "The largest palindrome made from the product of two 3-digit numbers is 906609" do
+    assert PE.Palindrome.max_palindrome_of_three_digit_pair_product() == 906_609
   end
 end
