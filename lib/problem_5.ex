341bfd636beb2b2272c97837670ec49953cb2759
goggalore <junec177@gmail.com>
2020-07-19 15:26:54 -0700

    update problem 5 with an optimized solution

--- lib/problem_5.ex
+++ lib/problem_5.ex
@@ -11,20 +11,28 @@ defmodule PE.RangeDivisible do
   of the numbers from 1 to 20?
   """
 
-  @spec find(Range) :: integer
-  def find(range) do
-    initial = Enum.fetch!(range, length(range |> Enum.to_list()) - 1) * 2
-    find(range, initial)
+  # heavily uses the fundimental theorem of algebra
+  @spec find(non_neg_integer) :: integer
+  def find(upper_bound) do
+    primes = Prime.get_primes_with_bound(upper_bound)
+    limit = :math.sqrt(upper_bound)
+    find(upper_bound, primes, limit, 1)
   end
 
-  defp find(range, n) do
-    divisible_by_all? =
-      range
-      |> Enum.all?(&Math.multiple?(n, &1))
+  defp find(upper_bound, primes, limit, result) do
+    case primes do
+      [] ->
+        trunc(result)
 
-    case divisible_by_all? do
-      true -> n
-      false -> find(range, n + 1)
+      [prime | remaining_primes] ->
+        if prime < limit do
+          prime_power = trunc(:math.log(upper_bound) / :math.log(prime))
+          new_result = result * :math.pow(prime, prime_power)
+          find(upper_bound, remaining_primes, limit, new_result)
+        else
+          new_result = result * prime
+          find(upper_bound, remaining_primes, limit, new_result)
+        end
     end
   end
 end
