b3db68764a394c276f78c54d798e46338b0f5aa2
goggalore <junec177@gmail.com>
2020-07-23 17:57:11 -0700

    optimize solution and massively increase code clarity for problem 9

--- lib/problem_9.ex
+++ lib/problem_9.ex
@@ -22,103 +22,42 @@ defmodule PE.SpecialPythagoreanTriplet do
 
   @spec find_triplet(any) ::
           {:error, String.t()} | {:ok, {pos_integer, pos_integer, pos_integer}}
-  def find_triplet(n) when n < 0, do: {:error, "argument must be a positive number"}
+
+  def find_triplet(n) when n < 0 or not is_integer(n),
+    do: {:error, "argument must be a positive number"}
+
   def find_triplet(0), do: {:ok, {0, 0, 0}}
 
   def find_triplet(n) do
-    find_triplet({1, 1, 1}, n)
-  end
+    no_sol = {:error, "No such triplet exists where a, b, c are all positive integers"}
+    a_n = 3..div(n - 3, 3)
 
-  defp find_triplet(triplet, n) do
-    {a, b, c} = triplet
+    a_n
+    |> Enum.reduce_while(no_sol, fn a, _ ->
+      b_n = (a + 1)..div(n - 1 - a, 2)
 
-    if c < n do
-      case incr_b(triplet, n) do
-        {true, t} -> {:ok, t}
-        {false, _} -> find_triplet({a, b, c + 1}, n)
+      {status, triplet} =
+        b_n
+        |> Enum.reduce_while(no_sol, fn b, _ ->
+          c = n - a - b
+
+          if solution?({a, b, c}, n) do
+            {:halt, {:ok, {a, b, c}}}
+          else
+            {:cont, no_sol}
+          end
+        end)
+
+      case status do
+        :ok -> {:halt, {:ok, triplet}}
+        :error -> {:cont, no_sol}
       end
-    else
-      {:error, "No such triplet exists where a, b, c are all positive integers"}
-    end
-  end
-
-  defp incr_b(triplet, n) do
-    {a, b, c} = triplet
-
-    case incr_a(triplet, n) do
-      {true, t} ->
-        {true, t}
-
-      {false, _} when b < c ->
-        incr_b({a, b + 1, c}, n)
-
-      {false, _} ->
-        {false, triplet}
-    end
-  end
-
-  defp incr_a(triplet, n) do
-    if solution?(triplet, n) do
-      {true, triplet}
-    else
-      {a, b, c} = triplet
-      if a < c, do: incr_a({a + 1, b, c}, n), else: {false, triplet}
-    end
+    end)
   end
 
   defp solution?({a, b, c}, n) do
-    pythagorean? = :math.pow(a, 2) + :math.pow(b, 2) == :math.pow(c, 2)
+    pythagorean? = a * a + b * b == c * c
     additive? = a + b + c == n
     pythagorean? and additive?
   end
 end
-
-"""
-3^2 + 4^2 = 5^2 = 25
-
-let a + b + c = 12
-where a^2 + b^2 = c^2
-
-(a + b + c)^2 = 12^2
-a^2 + 2ab + 2ac + b^2 + 2bc + c^2 = 144
-a^2 + 2b(a + c) + 2ac + b^2 + c^2 = 144
-a^2 + 2b(a + c) + 2ac + c^2 - a^2 + c^2 = 144
-2b(a + c) + 2ac + 2c^2 = 144
-2b(a + c) + 2(c - b)c + 2c^2 = 144
-2b(a + c) - bc + 4c^2 = 144
-2ab + bc + 4c^2 = 144
-
-c^2 + bc + ac = 12c
-bc = 12c - ac - c^2
-
-2ab + 12c - ac - c^2 + 4c^2 = 144
-2ab - ac + 3c^2 + 12c = 144
-a(2b - c) + 3c^2 + 12c = 144
-
-a = 12 - b - c
-
-(12 - b - c)(2b - c) + 3c^2 + 12c = 144
-24b - 12c - 2b^2 + bc - 2cb + c^2 + 3c^2 + 12c = 144
-
-
-can use system elimination now?
-24b - 2b^2 - bc + 4c^2 = 144
-a + b + c = 12
-a^2 + b^2 = c^2
-
-ab + b^2 + bc - 12b = 0
-
-2ab + 2b^2 + 2bc - 24b = 0
-24b - 2b^2 + bc + 4c^2 = 144
-=
-2ab + 3bc + 4c^2 = 144
-
-
-
-
-let a + b + c = n
-where a^2 + b^2 = c^2
-
-a(a + 1) + b(b + 1) + c(1 - c) = n
-
-"""
