85beb0e7a007c51a2c9a56020d4fd0a31f409796
goggalore <junec177@gmail.com>
2020-07-27 16:51:25 -0700

    add initial (broken) solution for problem 8

---
+++ lib/problem_8.ex
@@ -0,0 +1,28 @@
+defmodule PE.SeriesProduct do
+  require Integer
+
+  def largest_product(number, size) do
+    sequence = Integer.digits(number)
+
+    sub_sequences = all_sub_sequences(sequence, size, [])
+
+    IO.puts(sub_sequences)
+
+    sub_sequences
+    |> Enum.map(fn sub_sequence -> Enum.reduce(sub_sequence, &*/2) end)
+    |> Enum.max()
+  end
+
+  defp all_sub_sequences(sequence, size, acc) do
+    {sub_sequence, _} =
+      sequence
+      |> Enum.split(size)
+
+    if length(sub_sequence) < size do
+      acc
+    else
+      [_ | tail] = sequence
+      all_sub_sequences(tail, size, [sub_sequence | acc])
+    end
+  end
+end
