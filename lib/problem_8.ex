dfe8b34cd7aef5007abcca2e067fbf86468df110
goggalore <junec177@gmail.com>
2020-07-27 18:26:47 -0700

    add documentation to problem_8.ex

--- lib/problem_8.ex
+++ lib/problem_8.ex
@@ -1,4 +1,22 @@
 defmodule PE.SeriesProduct do
+  @doc """
+
+  The four adjacent digits in the 1000-digit
+  number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+
+  [the 1000 digit number can be seen in problem_8_test.exs]
+
+
+  ## Examples
+
+    iex> PE.SeriesProduct.largest_product(large_number, 4)
+    5832
+
+    iex> PE.SeriesProduct.largest_product(large_number, 13)
+    23514624000
+
+  """
+
   require Integer
 
   def largest_product(number, size) do
