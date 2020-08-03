5862582b9303c61bf1b21ecda0ff3ad8981228c1
goggalore <junec177@gmail.com>
2020-08-03 15:37:16 -0700

    add triangle numbers module

---
+++ lib/triangle_numbers.ex
@@ -0,0 +1,9 @@
+defmodule TriangleNumbers do
+  @spec get_term(integer) :: integer
+  def get_term(n) when n < 0, do: 0
+  def get_term(1), do: 1
+
+  def get_term(n) do
+    n + get_term(n - 1)
+  end
+end
