ad70bcbe505add23d127b0a8ae4e8416adfa8d6a
goggalore <junec177@gmail.com>
2020-08-04 16:07:37 -0700

    add prime_factors function which gets all the prime factors of a number and their powers

--- lib/math.ex
+++ lib/math.ex
@@ -23,4 +23,41 @@ defmodule Math do
       multiples(n, bound, i + 1, [result | accum])
     end
   end
+
+  @spec prime_factors(non_neg_integer) :: [{non_neg_integer, non_neg_integer}]
+  def prime_factors(n) do
+    prime_index = 0
+    prime_factors(n, Prime.get_prime(prime_index), prime_index, 0, [])
+  end
+
+  defp prime_factors(n, current_prime, prime_index, current_pow, prime_factors) do
+    new_pf =
+      if current_pow != 0,
+        do: [{current_prime, current_pow} | prime_factors],
+        else: prime_factors
+
+    if n == 1 do
+      new_pf
+    else
+      if rem(n, current_prime) == 0 do
+        prime_factors(
+          div(n, current_prime),
+          current_prime,
+          prime_index,
+          current_pow + 1,
+          prime_factors
+        )
+      else
+        new_prime_index = prime_index + 1
+
+        prime_factors(
+          n,
+          Prime.get_prime(new_prime_index),
+          new_prime_index,
+          0,
+          new_pf
+        )
+      end
+    end
+  end
 end
