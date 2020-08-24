f25d3733cd6515fffc37399810410a8e5c5a3cbc
goggalore <junec177@gmail.com>
2020-08-24 15:09:12 -0700

    remove triangle_numbers module

--- lib/triangle_numbers.ex
+++
@@ -1,9 +0,0 @@
-defmodule TriangleNumbers do
-  @spec get_term(integer) :: integer
-  def get_term(n) when n < 1, do: 0
-  def get_term(1), do: 1
-
-  def get_term(n) do
-    n + get_term(n - 1)
-  end
-end
